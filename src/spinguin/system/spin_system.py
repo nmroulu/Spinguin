cc611274c22790ad530a620c5df83636a50e37fc
Joni Eronen <joni.eronen@oulu.fi>
2025-05-06 09:00:11 +0300

    Accepts any ArrayLike as input

--- src/spinguin/system/spin_system.py
+++ src/spinguin/system/spin_system.py
@@ -9,10 +9,12 @@ other modules can be used to calculate its properties.
 from __future__ import annotations
 import numpy as np
 import hashlib
+from numpy.typing import ArrayLike
 from pickle import dumps
 from spinguin.utils.nmr_isotopes import ISOTOPES
 from spinguin.utils.data_io import read_array, read_tensors, read_xyz
 from spinguin.system.basis import Basis
+from spinguin.utils.la import arraylike_to_array
 
 class SpinSystem:
 
@@ -24,12 +26,12 @@ class SpinSystem:
 
     def __init__(
         self, 
-        isotopes: np.ndarray | str, 
-        chemical_shifts: np.ndarray | str = None, 
-        J_couplings: np.ndarray | str = None,
-        xyz: np.ndarray | str = None,
-        shielding: np.ndarray | str = None,
-        efg: np.ndarray | str = None,
+        isotopes: ArrayLike | str, 
+        chemical_shifts: ArrayLike | str = None, 
+        J_couplings: ArrayLike | str = None,
+        xyz: ArrayLike | str = None,
+        shielding: ArrayLike | str = None,
+        efg: ArrayLike | str = None,
         tau_c: float = None,
         max_spin_order: int = None):
         """
@@ -37,35 +39,36 @@ class SpinSystem:
 
         Parameters
         ----------
-        isotopes : numpy.ndarray or str
+        isotopes : ArrayLike or str
             Specifies the isotopes that constitute the spin system and determine other properties,
             such as spin quantum numbers and gyromagnetic ratios.
 
-            - If a `numpy.ndarray`: A 1D array of size N containing isotope names as strings. Example:
+            - If `ArrayLike`: A 1D array of size N containing isotope names as strings. Example:
 
             ```python
             np.array(['1H', '15N', '19F'])
             ```
 
-            - If a `str`: Path to the file containing the isotopes.
+            - If `str`: Path to the file containing the isotopes.
         
-        chemical_shifts : numpy.ndarray or str
+        chemical_shifts : ArrayLike or str
             Chemical shifts arising from the isotropic component of the nuclear shielding tensors.
-            Used when calculating the coherent Hamiltonian.
+            Used when calculating the coherent Hamiltonian. If left empty, zero ppm is assumed for
+            each spin in the system.
 
-            - If a `numpy.ndarray`: A 1D array of size N containing the chemical shifts in ppm. Example:
+            - If `ArrayLike`: A 1D array of size N containing the chemical shifts in ppm. Example:
 
             ```python
             np.array([8.00, -200, -130])
             ```
 
-            - If a `str`: Path to the file containing the chemical shifts.
+            - If `str`: Path to the file containing the chemical shifts.
 
-        J_couplings : numpy.ndarray or str
+        J_couplings : ArrayLike or str
             Specifies the scalar coupling constants between each spin pair in the spin system. Used when
             calculating the coherent Hamiltonian.
 
-            - If a `numpy.ndarray`: A 2D array of size (N, N) specifying the scalar couplings between nuclei in Hz.
+            - If `ArrayLike`: A 2D array of size (N, N) specifying the scalar couplings between nuclei in Hz.
             Only the lower triangle is specified. Example:
 
             ```python
@@ -76,28 +79,28 @@ class SpinSystem:
             ])
             ```
 
-            - If a `str`: Path to the file containing the scalar couplings.
+            - If `str`: Path to the file containing the scalar couplings.
 
-        xyz : numpy.ndarray or str
+        xyz : ArrayLike or str
             Coordinates in the XYZ format for each nucleus in the spin system. Used in relaxation when calculating
             the dipole-dipole coupling tensors.  
         
-            - If a `numpy.ndarray`: A 2D array of size (N, 3) containing the Cartesian coordinates in Å.
-            - If a `str`: Path to the file containing the XYZ coordinates.
+            - If `ArrayLike`: A 2D array of size (N, 3) containing the Cartesian coordinates in Å.
+            - If `str`: Path to the file containing the XYZ coordinates.
 
-        shielding : numpy.ndarray or str
+        shielding : ArrayLike or str
             Specifies the nuclear shielding tensors for each nucleus. Note that the isotropic part of the tensor
             is handled by `chemical_shifts`. The shielding tensors are used only for relaxation.
 
-            - If a `numpy.ndarray`: A 3D array of size (N, 3, 3) containing the 3x3 shielding tensors in ppm.
-            - If a `str`: Path to the file containing the shielding tensors.
+            - If `ArrayLike`: A 3D array of size (N, 3, 3) containing the 3x3 shielding tensors in ppm.
+            - If `str`: Path to the file containing the shielding tensors.
 
-        efg : numpy.ndarray or str
+        efg : ArrayLike or str
             Electric field gradient tensors used for incorporating the quadrupolar interaction relaxation
             mechanism.
 
-            - If a `numpy.ndarray`: A 3D array of size (N, 3, 3) containing the 3x3 EFG tensors in atomic units.
-            - If a `str`: Path to the file containing the EFG tensors.
+            - If `ArrayLike`: A 3D array of size (N, 3, 3) containing the 3x3 EFG tensors in atomic units.
+            - If `str`: Path to the file containing the EFG tensors.
 
         tau_c : float
             Isotropic rotational correlation time in units of s. Must be defined to use
@@ -111,62 +114,68 @@ class SpinSystem:
         """
 
         # Assign isotopes
-        if isinstance(isotopes, np.ndarray):
-            self.isotopes = isotopes
-        elif isinstance(isotopes, str):
+        if isinstance(isotopes, str):
             self.isotopes = read_array(isotopes, data_type=str)
         else:
-            raise TypeError("Isotopes should be a NumPy array or a string.")
+            try:
+                self.isotopes = arraylike_to_array(isotopes)
+            except Exception:
+                raise TypeError("Isotopes should be a 1-dimensional array or a string.")
         
         # Assign chemical shifts
-        if isinstance(chemical_shifts, np.ndarray):
-            self.chemical_shifts = chemical_shifts
-        elif isinstance(chemical_shifts, str):
+        if isinstance(chemical_shifts, str):
             self.chemical_shifts = read_array(chemical_shifts, data_type=float)
         elif chemical_shifts is None:
             self.chemical_shifts = np.zeros(self.size, dtype=float)
         else:
-            raise TypeError("Chemical shifts should be a NumPy array, a string, or None.")
+            try:
+                self.chemical_shifts = arraylike_to_array(chemical_shifts)
+            except Exception:
+                raise TypeError("Chemical shifts should be a 1-dimensional array, a string, or None.")
 
         # Assign scalar couplings
-        if isinstance(J_couplings, np.ndarray):
-            self.J_couplings = J_couplings
-        elif isinstance(J_couplings, str):
+        if isinstance(J_couplings, str):
             self.J_couplings = read_array(J_couplings, data_type=float)
         elif J_couplings is None:
             self.J_couplings = np.zeros((self.size, self.size), dtype=float)
         else:
-            raise TypeError("Scalar couplings should be a NumPy array, a string, or None.")
+            try:
+                self.J_couplings = arraylike_to_array(J_couplings)
+            except Exception:
+                raise TypeError("J-couplings should be a 2-dimensional array, a string, or None.")
         
         # Assign XYZ coordinates
-        if isinstance(xyz, np.ndarray):
-            self.xyz = xyz
-        elif isinstance(xyz, str):
+        if isinstance(xyz, str):
             self.xyz = read_xyz(xyz)
         elif xyz is None:
             self.xyz = None
         else:
-            raise TypeError("XYZ should be a NumPy array, a string, or None.")
+            try:
+                self.xyz = arraylike_to_array(xyz)
+            except Exception:
+                raise TypeError("XYZ should be a 2-dimensional array, a string, or None.")
         
         # Assign shielding tensors
-        if isinstance(shielding, np.ndarray):
-            self.shielding = shielding
-        elif isinstance(shielding, str):
+        if isinstance(shielding, str):
             self.shielding = read_tensors(shielding)
         elif shielding is None:
             self.shielding = None
         else:
-            raise TypeError("Shielding should be a NumPy array, a string, or None.")
+            try:
+                self.shielding = arraylike_to_array(shielding)
+            except Exception:
+                raise TypeError("Shielding should be a 3-dimensional array, a string, or None.")
         
         # Assign EFG tensors
-        if isinstance(efg, np.ndarray):
-            self.efg = efg
-        elif isinstance(efg, str):
+        if isinstance(efg, str):
             self.efg = read_tensors(efg)
         elif efg is None:
             self.efg = None
         else:
-            raise TypeError("EFG should be a NumPy array, a string, or None.")
+            try:
+                self.efg = arraylike_to_array(efg)
+            except Exception:
+                raise TypeError("EFG should be a 3-dimensional array, a string, or None.")
         
         # Assign correlation time
         if isinstance(tau_c, float) or tau_c is None:
