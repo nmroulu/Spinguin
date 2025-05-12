f5c292abe9795fb7c52553479e716821a4d1206d
Joni Eronen <joni.eronen@oulu.fi>
2025-05-12 17:00:26 +0300

    Hide the functionality under spinguin namespace

--- src/spinguin/__init__.py
+++ src/spinguin/__init__.py
@@ -1,25 +1,2 @@
 # Make the functions and classes accessible directly from the Spinguin package
 # TODO: Reveal only the important functions / classes
-
-# qm module
-from spinguin.qm.chem import *
-from spinguin.qm.hamiltonian import *
-from spinguin.qm.liouvillian import *
-from spinguin.qm.operators import *
-from spinguin.qm.propagation import *
-from spinguin.qm.relaxation import *
-from spinguin.qm.states import *
-from spinguin.qm.superoperators import *
-
-# system module
-from spinguin.system.basis import *
-from spinguin.system.spin_system import *
-from spinguin.system.composite_spin_system import *
-
-# utils module
-from spinguin.utils.data_io import *
-from spinguin.utils.la import *
-from spinguin.utils.nmr_isotopes import *
-
-# config
-from spinguin.config import *
