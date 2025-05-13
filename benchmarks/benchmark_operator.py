eb37915999a7ab7ed8a99d0daa8854f05264a460
Joni Eronen <joni.eronen@oulu.fi>
2025-05-13 16:24:23 +0300

    Include only average times and have slightly larger default test

--- benchmarks/benchmark_operator.py
+++ benchmarks/benchmark_operator.py
@@ -7,7 +7,7 @@ This benchmark is useful for comparing whether to use the dense
 or sparse formalism for creating operators.
 
 On a laptop with 11th gen. i5 processor and 16 GB ram, this bench-
-mark takes a few seconds to run with `max_nspins=12`, with sparse
+mark takes a few minutes to run with `max_nspins=14`, with sparse
 arrays becoming faster at ~10 spins.
 """
 
@@ -18,14 +18,10 @@ from time import perf_counter
 from spinguin.qm.operators import op_prod
 
 # Testing parameters
-max_nspins = 12
+max_nspins = 14
 
 # Create empty arrays for the testing results
-min_dense = np.empty(max_nspins, dtype=float)
-max_dense = np.empty(max_nspins, dtype=float)
 avg_dense = np.empty(max_nspins, dtype=float)
-min_sparse = np.empty(max_nspins, dtype=float)
-max_sparse = np.empty(max_nspins, dtype=float)
 avg_sparse = np.empty(max_nspins, dtype=float)
 
 # Test with various spin system sizes
@@ -45,57 +41,35 @@ for nspins in range(1, max_nspins+1):
             opers.append(op)
     nopers = len(opers)
 
-    # Minimum and maximum runtimes
-    min_dn = np.inf
-    max_dn = 0
+    # Total runtimes
     tot_dn = 0
-    min_sp = np.inf
-    max_sp = 0
     tot_sp = 0
 
     # Test with every product operator
     for op_def in opers:
-        op_def = np.array(op_def)
 
         # Test with dense arrays
         ts = perf_counter()
         op_prod(op_def, spins, include_unit=True, sparse=False)
         te = perf_counter()
-        t = te-ts
-        tot_dn += t
-        if t < min_dn:
-            min_dn = t
-        if t > max_dn:
-            max_dn = t
+        tot_dn += te-ts
 
         # Test with sparse arrays
         ts = perf_counter()
         op_prod(op_def, spins, include_unit=True, sparse=True)
         te = perf_counter()
-        t = te-ts
-        tot_sp += t
-        if t < min_sp:
-            min_sp = t
-        if t > max_sp:
-            max_sp = t
+        tot_sp += te-ts
 
     # Save the results
-    min_dense[nspins-1] = min_dn
-    max_dense[nspins-1] = max_dn
     avg_dense[nspins-1] = tot_dn/nopers
-    min_sparse[nspins-1] = min_sp
-    max_sparse[nspins-1] = max_sp
     avg_sparse[nspins-1] = tot_sp/nopers
 
 # Plot the results
-nspins = np.linspace(1, nspins, num=nspins)
-plt.plot(nspins, min_dense, label="Dense: MIN")
-plt.plot(nspins, max_dense, label="Dense: MAX")
-plt.plot(nspins, avg_dense, label="Dense: AVG")
-plt.plot(nspins, min_sparse, label="Sparse: MIN")
-plt.plot(nspins, max_sparse, label="Sparse: MAX")
-plt.plot(nspins, avg_sparse, label="Sparse: AVG")
+nspins = np.linspace(1, max_nspins, num=max_nspins)
+plt.plot(nspins, avg_dense, label="Dense")
+plt.plot(nspins, avg_sparse, label="Sparse")
 plt.xlabel("Number of spins")
 plt.ylabel("Time (s)")
 plt.legend()
+plt.tight_layout()
 plt.show()
