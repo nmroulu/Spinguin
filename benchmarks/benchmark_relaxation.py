017e680481a7d179db3b86dca4d17cd2ac7063f0
Joni Eronen <joni.eronen@oulu.fi>
2025-05-20 15:45:31 +0300

    Simple benchmark for constructing R for arbitrary spin system size

---
+++ benchmarks/benchmark_relaxation.py
@@ -0,0 +1,63 @@
+# Imports
+import numpy as np
+from spinguin.utils.nmr_isotopes import ISOTOPES
+from spinguin.qm.hamiltonian import sop_H_coherent
+from spinguin.qm.relaxation import sop_R_redfield
+from spinguin.qm.basis import make_basis
+
+# Testing parameters
+nspins = 12
+max_spin_order = 3
+
+# Set magnetic field
+B = 1
+
+# Set correlation time
+tau_c = 50e-12
+
+# Create the spin system
+spins = np.array([1/2 for _ in range(nspins)])
+basis = make_basis(spins, max_spin_order)
+
+# Obtain the gyromagnetic ratios
+y1H = 2*np.pi * ISOTOPES['1H'][1] * 1e6
+gammas = np.array([y1H for _ in range(nspins)])
+quad = np.array([0 for _ in range(nspins)])
+
+# Create some geometry
+xyz = []
+for spin in range(nspins):
+    theta = spin * 2.399963
+    z = 1.5 * (spin / nspins - 0.5)
+    r = 1.5 * np.sqrt(spin)
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    xyz.append([x, y, z])
+xyz = np.array(xyz)
+
+# Get a distance array
+connectors = xyz[:, np.newaxis] - xyz
+distances = np.linalg.norm(connectors, axis=2)
+
+# Create some chemical shifts
+chemical_shifts = np.array([5 + 0.25*i*(-1)**i for i in range(nspins)])
+
+# Create some J-couplings based on distances
+J_couplings = np.zeros((nspins, nspins))
+for i in range(nspins):
+    for j in range(i):
+        J_couplings[i,j] = 20 / distances[i,j]**3
+
+# Create the Hamiltonian superoperator
+sop_H = sop_H_coherent(basis, gammas, spins, chemical_shifts, J_couplings, B,
+                       "comm", True, 1e-12)
+
+# Benchmark the Redfield superoperator
+sop_R_redfield(basis = basis,
+               sop_H = sop_H,
+               tau_c = tau_c,
+               spins = spins,
+               B = B,
+               gammas = gammas,
+               xyz = xyz)
+
