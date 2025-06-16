cc89f318e5011d67c7391c078e9d340bf78da641
Joni Eronen <joni.eronen@oulu.fi>
2025-06-16 15:35:05 +0300

    Unify the parameters into one file

--- src/spinguin/api/parameters.py
+++ src/spinguin/api/parameters.py
@@ -5,53 +5,87 @@ This module provides the Parameters class which contains all the necessary
 global parameters required for spin dynamics simulations.
 """
 
-# Imports
-import numpy as np
-from spinguin.core.la import arraylike_to_array
-
 class Parameters:
     """
-    Parameters class contains all the necessary global parameters required for
-    spin dynamics simulations.
+    Parameters class contains all the global settings for the Spinguin package.
 
     Attributes
     ----------
-    magnetic_field : float, default=None
-        External magnetic field in the units of T.
-    temperature : float, default=None
-        Temperature in the units of K.
-    center_frequency : dict, default=None
-        Defines the central point of the frequency window for data acquisition.
-        Center frequency, defined in ppm, can be set for each isotope
-        inividually.
-    dwell_time : ndarray, default=None
-        Defines the sampling interval in seconds. If there are multiple
-        dimensions, the direct dimension is always specified last. Dwell time
-        can also be set by changing the spectral width.
-    spectral_width : ndarray, default=None
-        Defines the spectral width in Hz. If there are multiple dimensions, the
-        direct dimension is always specified last. Spectral width can also be
-        set by changing the dwell time.
-    npoints : int, default=None
-        Defines the number of points to acquire in the FID.
+    custom_dot : bool, default=False
+        Specifies whether to use custom matrix product implementation in the
+        time propagators. This implementation is parallelized and more memory-
+        friendly than the default SciPy implementation, but suffers from worse 
+        single-core performance.
+    propagator_density : float, default=0.5
+        Threshold that specifies when to use dense or sparse arrays for the
+        propagators.
+    sparse_hamiltonian : bool, default=True
+        Specifies whether to use sparse or dense arrays for Hamiltonian.
+    sparse_operator : bool, default=True
+        Specifies whether to use sparse or dense arrays for operators.
+    sparse_pulse : bool, default=True
+        Specifies whether to use sparse or dense arrays for pulses.
+    sparse_relaxation : bool, default=True
+        Specifies whether to use sparse or dense arrays for relaxation
+        superoperator.
+    sparse_state : bool, default=False
+        Specifies whether to use sparse or dense arrays for states.
+    sparse_superoperator : bool, default=True
+        Specifies whether to use sparse or dense arrays for superoperators.
+    zero_aux : float, default=1e-18
+        Threshold under which a value is considered to be zero in auxiliary
+        matrix method which is used in the Redfield relaxation theory.
+    zero_equilibrium : float, default=1e-18
+        Threshold under which a value is considered to be zero when performing
+        the matrix exponential while constructing the equilibrium state.
+    zero_hamiltonian : float, default=1e-12
+        Threshold under which a value is considered to be zero in Hamiltonian.
+    zero_interaction : float, default=1e-9
+        If the 1-norm of an interaction tensor (upper bound for its eigenvalues)
+        is smaller than this threshold, the interaction is ignored when
+        constructing the Redfield relaxation superoperator.
+    zero_propagator : float, default=1e-18
+        Threshold under which a value is considered to be zero in propagator.
+    zero_pulse : float, default=1e-18
+        Threshold under which a value is considered to be zero in pulse.
+    zero_relaxation : float, default=1e-12
+        Threshold under which a value is considered to be zero in relaxation
+        superoperator.
+    zero_thermalization : float, default=1e-18
+        Threshold under which a value is considered to be zero when performing
+        the matrix exponential while applying the Levitt-di Bari thermalization.
     """
 
     # Experimental conditions
     _magnetic_field: float = None
     _temperature: float = None
 
-    # Spectrometer settings
-    _center_frequency: dict = None
-    _dwell_time: np.ndarray = None
-    _spectral_width: np.ndarray = None
-    _isotope: np.ndarray = None
-    _npoints: np.ndarray = None
-    _angle: np.ndarray = None
+    # Matrix product settings
+    _custom_dot: bool=False
+
+    # Sparsity settings
+    _propagator_density: float=0.5
+    _sparse_hamiltonian: bool=True
+    _sparse_operator: bool=True
+    _sparse_pulse: bool=True
+    _sparse_relaxation: bool=True
+    _sparse_state: bool=False
+    _sparse_superoperator: bool=True
+    
+    # Zero-value thresholds
+    _zero_aux: float = 1e-18
+    _zero_equilibrium: float = 1e-18
+    _zero_hamiltonian: float = 1e-12
+    _zero_interaction: float = 1e-9
+    _zero_propagator: float = 1e-18
+    _zero_pulse: float = 1e-18
+    _zero_relaxation: float = 1e-12
+    _zero_thermalization: float = 1e-18
 
     @property
     def magnetic_field(self) -> float:
         return self._magnetic_field
-    
+
     @magnetic_field.setter
     def magnetic_field(self, magnetic_field: float):
         """
@@ -73,132 +107,218 @@ class Parameters:
         print(f"Temperature set to: {self.temperature} K\n")
 
     @property
-    def center_frequency(self) -> dict:
-        return self._center_frequency
+    def custom_dot(self) -> bool:
+        return self._custom_dot
     
-    @center_frequency.setter
-    def center_frequency(self, center_frequency: dict):
+    @custom_dot.setter
+    def custom_dot(self, custom_dot: bool):
         """
-        Defines the central point of the frequency window for data acquisition.
-        Center frequency, defined in ppm, can be set for each isotope
-        inividually. The input should be a dictionary.
+        Specifies whether to use custom matrix product implementation in the
+        time propagators. This implementation is parallelized and more memory-
+        friendly than the default SciPy implementation, but suffers from worse 
+        single-core performance.
         """
-        if not isinstance(center_frequency, dict):
-            raise ValueError("Center frequencies should be given as "
-                             "dictionary.")
-        if self._center_frequency is None:
-            self._center_frequency = center_frequency
-        else:
-            self._center_frequency.update(center_frequency)
-        print(f"Center frequencies set to: {self.center_frequency} (ppm)\n")
+        self._custom_dot = custom_dot
+        print(f"Custom dot product setting set to: {self.custom_dot}\n")
 
     @property
-    def dwell_time(self) -> np.ndarray:
-        return self._dwell_time
+    def sparse_operator(self) -> bool:
+        return self._sparse_operator
     
-    @dwell_time.setter
-    def dwell_time(self, dwell_time: float | list | tuple | np.ndarray):
+    @sparse_operator.setter
+    def sparse_operator(self, sparse_operator: bool):
         """
-        Defines the sampling interval in seconds. If multiple dimensions are
-        used, an array must be specified where the direct dimension corresponds
-        to the latest value.
+        Specifies whether to use sparse or dense arrays for operators.
         """
-        # Set the dwell time
-        self._dwell_time = arraylike_to_array(dwell_time)
-
-        # Set the spectral width accordingly
-        self._spectral_width = 1/self.dwell_time
-
-        print("Dwell time has been set to:")
-        for dim, dt in enumerate(self.dwell_time):
-            print(f"Dimension {dim+1}: {dt} s")
-        print()
-
-        print("Corresponding spectral widths are:")
-        for dim, sw in enumerate(self.spectral_width):
-            print(f"Dimension {dim+1}: {sw} Hz")
-        print()
+        self._sparse_operator = sparse_operator
+        print(f"Sparity setting of operator set to: {self.sparse_operator}\n")
 
     @property
-    def spectral_width(self) -> np.ndarray:
-        return self._spectral_width
+    def sparse_pulse(self) -> bool:
+        return self._sparse_pulse
     
-    @spectral_width.setter
-    def spectral_width(self, spectral_width: float | list | tuple | np.ndarray):
+    @sparse_pulse.setter
+    def sparse_pulse(self, sparse_pulse: bool):
         """
-        Defines the spectral width in Hz. If multiple dimensions are used, an
-        array must be specified where the latest element corresponds to the
-        direct dimension.
+        Specifies whether to use sparse or dense arrays for pulse
+        superoperarors.
         """
-        # Set the spectral width
-        self._spectral_width = arraylike_to_array(spectral_width)
-
-        # Set the dwell time accordingly
-        self._dwell_time = 1 / self.spectral_width
-
-        print("Spectral width has been set to:")
-        for dim, sw in enumerate(self.spectral_width):
-            print(f"Dimension {dim+1}: {sw} Hz")
-        print()
-
-        print("Corresponding dwell times are:")
-        for dim, dt in enumerate(self.dwell_time):
-            print(f"Dimension {dim+1}: {dt} s")
-        print()
+        self._sparse_pulse = sparse_pulse
+        print(f"Sparsity setting of pulses set to: {self.pulse}\n")
 
     @property
-    def isotope(self) -> np.ndarray:
-        return self._isotope
+    def sparse_superoperator(self) -> bool:
+        return self._sparse_superoperator
     
-    @isotope.setter
-    def isotope(self, isotope: str | list | tuple | np.ndarray):
+    @sparse_superoperator.setter
+    def sparse_superoperator(self, sparse_superoperator: bool):
         """
-        Defines the isotope to be measured. If multiple dimensions are used, an
-        array must be specified where the latest element corresponds to the
-        direct dimension.
+        Specifies whether to use sparse or dense arrays for superoperators.
         """
-        # Set the isotope
-        self._isotope = arraylike_to_array(isotope)
-        print("Isotope has been set to:")
-        for dim, iso in enumerate(self.isotope):
-            print(f"Dimension {dim+1}: {iso}")
-        print()
+        self._sparse_superoperator = sparse_superoperator
+        print("Sparity setting of superoperator set to: "
+              f"{self.sparse_superoperator}\n")
 
     @property
-    def npoints(self) -> np.ndarray:
-        return self._npoints
+    def sparse_hamiltonian(self) -> bool:
+        return self._sparse_hamiltonian
     
-    @npoints.setter
-    def npoints(self, npoints: int | list | tuple | np.ndarray):
+    @sparse_hamiltonian.setter
+    def sparse_hamiltonian(self, sparse_hamiltonian: bool):
         """
-        Defines the number of points to acquire in the FID. If multiple
-        dimensions are used, an array must be specified where the latest
-        element corresponds to the direct dimension.
+        Specifies whether to use sparse or dense arrays for Hamiltonian.
         """
-        # Set the number of points
-        self._npoints = arraylike_to_array(npoints)
-        print("Number of points has been set to:")
-        for dim, npo in enumerate(self.npoints):
-            print(f"Dimension {dim+1}: {npo}")
-        print()
+        self._sparse_hamiltonian = sparse_hamiltonian
+        print("Sparity setting of Hamiltonian set to: "
+              f"{self.sparse_hamiltonian}\n")
 
     @property
-    def angle(self) -> np.ndarray:
-        return self._angle
+    def sparse_relaxation(self) -> bool:
+        return self._sparse_relaxation
     
-    @angle.setter
-    def angle(self, angle: float | list | tuple | np.ndarray):
+    @sparse_relaxation.setter
+    def sparse_relaxation(self, sparse_relaxation: bool):
         """
-        Defines the angle of the magnetic field with respect to the sample.
-        If a pulse sequence requires more than one angle to be defined, an
-        array that specifies the angles must be given.
+        Specifies whether to use sparse or dense arrays for relaxation
+        superoperator.
         """
-        # Set the angle
-        self._angle = arraylike_to_array(angle)
-        print("Angle has been set to:")
-        for num, ang in enumerate(self.angle):
-            print(f"Angle {num+1}: {ang} degrees")
-        print()
+        self._sparse_relaxation = sparse_relaxation
+        print("Sparity setting of relaxation set to: "
+              f"{self.sparse_relaxation}\n")
+
+    @property
+    def sparse_state(self) -> bool:
+        return self._sparse_state
+    
+    @sparse_state.setter
+    def sparse_state(self, sparse_state: bool):
+        """
+        Specifies whether to use sparse or dense arrays for states.
+        """
+        self._sparse_state = sparse_state
+        print(f"Sparity setting of state set to: {self.sparse_state}\n")
+
+    @property
+    def propagator_density(self) -> float:
+        return self._propagator_density
+    
+    @propagator_density.setter
+    def propagator_density(self, propagator_density: float):
+        """
+        Threshold that specifies when to use dense or sparse arrays for the
+        propagators.
+        """
+        self._propagator_density = propagator_density
+        print("Propagator density threshold set to: "
+              f"{self.propagator_density}\n")
+
+    @property
+    def zero_hamiltonian(self) -> float:
+        return self._zero_hamiltonian
+    
+    @zero_hamiltonian.setter
+    def zero_hamiltonian(self, zero_hamiltonian: float):
+        """
+        Threshold under which a value is considered to be zero in Hamiltonian.
+        """
+        self._zero_hamiltonian = zero_hamiltonian
+        print("Hamiltonian zero-value threshold set to: "
+              f"{self.zero_hamiltonian}\n")
+
+    @property
+    def zero_aux(self) -> float:
+        return self._zero_aux
+    
+    @zero_aux.setter
+    def zero_aux(self, zero_aux: float):
+        """
+        Threshold under which a value is considered to be zero in auxiliary
+        matrix method which is used in the Redfield relaxation theory.
+        """
+        self._zero_aux = zero_aux
+        print(f"Auxiliary zero-value threshold set to: {self.zero_aux}\n")
+
+    @property
+    def zero_relaxation(self) -> float:
+        return self._zero_relaxation
+    
+    @zero_relaxation.setter
+    def zero_relaxation(self, zero_relaxation: float):
+        """
+        Threshold under which a value is considered to be zero in relaxation
+        superoperator.
+        """
+        self._zero_relaxation = zero_relaxation
+        print("Relaxation zero-value threshold set to: "
+              f"{self.zero_relaxation}\n")
+
+    @property
+    def zero_interaction(self) -> float:
+        return self._zero_interaction
+    
+    @zero_interaction.setter
+    def zero_interaction(self, zero_interaction: float):
+        """
+        If the 1-norm of an interaction tensor (upper bound for its eigenvalues)
+        is smaller than this threshold, the interaction is ignored when
+        constructing the Redfield relaxation superoperator.
+        """
+        self._zero_interaction = zero_interaction
+        print("Interaction zero-value threshold set to: "
+              f"{self.zero_interaction}\n")
+
+    @property
+    def zero_propagator(self) -> float:
+        return self._zero_propagator
+    
+    @zero_propagator.setter
+    def zero_propagator(self, zero_propagator: float):
+        """
+        Threshold under which a value is considered to be zero in propagator.
+        """
+        self._zero_propagator = zero_propagator
+        print("Propagator zero-value threshold set to: "
+              f"{self.zero_propagator}\n")
+
+    @property
+    def zero_pulse(self) -> float:
+        return self._zero_pulse
+    
+    @zero_pulse.setter
+    def zero_pulse(self, zero_pulse: float):
+        """
+        Threshold under which a value is considered to be zero in pulse.
+        """
+        self._zero_pulse = zero_pulse
+        print(f"Pulse zero-value threshold set to: {self.zero_pulse}\n")
+
+    @property
+    def zero_thermalization(self) -> float:
+        return self._zero_thermalization
+    
+    @zero_thermalization.setter
+    def zero_thermalization(self, zero_thermalization: float):
+        """
+        Threshold under which a value is considered to be zero when performing
+        the matrix exponential while applying the Levitt-di Bari thermalization.
+        """
+        self._zero_thermalization = zero_thermalization
+        print("Thermalization zero-value threshold set to: "
+              f"{self.zero_thermalization}\n")
+
+    @property
+    def zero_equilibrium(self) -> float:
+        return self._zero_equilibrium
+    
+    @zero_equilibrium.setter
+    def zero_equilibrium(self, zero_equilibrium: float):
+        """
+        Threshold under which a value is considered to be zero when performing
+        the matrix exponential while constructing the equilibrium state.
+        """
+        self._zero_equilibrium = zero_equilibrium
+        print("Equilibrium zero-value threshold set to: "
+              f"{self.zero_equilibrium}\n")
 
 # Instantiate the Parameters object
 parameters = Parameters()
