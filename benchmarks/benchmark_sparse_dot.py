e569f41e1d700914334f6b0ed84822ec45f85cb6
Joni Eronen <joni.eronen@oulu.fi>
2025-06-16 15:02:12 +0300

    Update the benchmark to be more fair between implementations

--- benchmarks/benchmark_sparse_dot.py
+++ benchmarks/benchmark_sparse_dot.py
@@ -1,15 +1,17 @@
 """
 This script benchmarks the performance of SciPy's sparse matrix multiplication
 against a custom sparse dot implementation for various matrix densities.
+
+NOTE: Takes several minutes to run!
 """
 
 # Imports
-from scipy.sparse import random_array
-from spinguin.core.la import sparse_dot
-from time import perf_counter
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.sparse import random_array
+from spinguin.core.la import sparse_dot
+from time import perf_counter
 
 # Densities to test (fraction of non-zero elements in the matrix)
 densities = np.array([0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5])
@@ -19,6 +21,9 @@ max_nnz = 1e6
 
 # Number of matrix dimensions to test for each density
 npoints = 30
+
+# Averaging
+nrounds = 100
 
 # Iterate over the specified densities
 for density in densities:
@@ -37,19 +42,23 @@ for density in densities:
     for i, dim in enumerate(dims):
 
         # Create a random sparse matrix with the given dimension and density
-        A = random_array((dim, dim), density=density, format='csc')
+        A_CSR = random_array((dim, dim), density=density, format='csr',
+                             dtype=complex)
+        A_CSC = A_CSR.tocsc()
 
         # Measure execution time for SciPy sparse matrix multiplication
-        time_start_csr_csr = perf_counter()
-        A @ A
-        time_end_csr_csr = perf_counter()
-        result_SciPy[i] = time_end_csr_csr - time_start_csr_csr
+        time_start_SciPy = perf_counter()
+        for _ in range(nrounds):
+            A_CSR @ A_CSR
+        time_end_SciPy = perf_counter()
+        result_SciPy[i] = (time_end_SciPy - time_start_SciPy)/nrounds
 
         # Measure execution time for the custom sparse dot implementation
         time_start_custom = perf_counter()
-        sparse_dot(A, A, zero_value=1e-32)
+        for _ in range(nrounds):
+            sparse_dot(A_CSC, A_CSC, zero_value=0)
         time_end_custom = perf_counter()
-        result_custom[i] = time_end_custom - time_start_custom
+        result_custom[i] = (time_end_custom - time_start_custom)/nrounds
 
     # Plot the benchmark results for the current density
     plt.plot(dims, result_SciPy, label='SciPy')
