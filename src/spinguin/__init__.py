4a700d16688ab2fa3655a8bda089e235a6f95ab4
Joni Eronen <joni.eronen@oulu.fi>
2025-06-17 14:36:34 +0300

    Organise the namespace a bit better

--- src/spinguin/__init__.py
+++ src/spinguin/__init__.py
@@ -1,4 +1,3 @@
 # Make functionality from the API accessible directly under the spinguin
 # namespace
-from spinguin.api.parameters import parameters
-from spinguin.api.core import *
+from spinguin.api import *
---
+++ src/spinguin/api/__init__.py
@@ -0,0 +1,35 @@
+# Expose only the necessary functions from the API
+from spinguin.api.core import (
+    alpha_state,
+    beta_state,
+    associate,
+    dissociate,
+    equilibrium_state,
+    frequency_to_chemical_shift,
+    hamiltonian,
+    inversion_recovery,
+    liouvillian,
+    measure,
+    operator,
+    permute_spins,
+    propagator,
+    propagator_to_rotframe,
+    pulse,
+    pulse_and_acquire,
+    relaxation,
+    resonance_frequency,
+    singlet_state,
+    spectral_width_to_dwell_time,
+    spectrum,
+    spin_system,
+    state,
+    state_to_zeeman,
+    superoperator,
+    time_axis,
+    triplet_minus_state,
+    triplet_plus_state,
+    triplet_zero_state,
+    unit_state
+)
+
+from spinguin.api.parameters import parameters
