ff74f28ffccdc69e3465e2605bc445864afa5f9a
Joni Eronen <joni.eronen@oulu.fi>
2025-11-08 15:10:50 +0200

    Add logo and version switcher

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'Spinguin'
 copyright = '2025, Joni Eronen, Perttu Hilla'
 author = 'Joni Eronen, Perttu Hilla'
-release = '0.0.1'
+release = 'latest'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,14 +24,31 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode'
 ]
-templates_path = ['_templates']
 exclude_patterns = []
 add_module_names = False
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "switcher": {
+        "json_url": "https://nmroulu.github.io/Spinguin/latest/_static/version_switcher.json",
+        "version_match": release
+    },
+    "logo": {
+        "text": "Spinguin documentation",
+        "image_light": "_static/spinguin_logo.png",
+        "image_dark": "_static/spinguin_logo.png",
+    }
+}
+html_sidebars = {
+    "**": [
+        "navbar-logo.html",
+        "version-switcher.html",
+        "icon-links.html",
+        "search-button-field.html",
+        "sbt-sidebar-nav.html"
+        ]
+}
 html_static_path = ['_static']
