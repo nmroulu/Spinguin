16a227799ee8a492067b4aa421d4264ee521c292
Joni Eronen <joni.eronen@oulu.fi>
2025-11-11 09:44:09 +0200

    Update hamiltonian tests to use the public APi

--- tests/core_tests/test_hamiltonian.py
+++ tests/core_tests/test_hamiltonian.py
@@ -1,37 +1,32 @@
 import unittest
 import numpy as np
 import os
-from scipy.sparse import load_npz
-from spinguin._core._hamiltonian import sop_H
-from spinguin._core._nmr_isotopes import ISOTOPES
-from spinguin._core._basis import make_basis
+from scipy.sparse import load_npz, csc_array
+import spinguin as sg
 
 class TestHamiltonian(unittest.TestCase):
-    """
-    Unit test for the Hamiltonian generation functionality.
-    """
 
+    # TODO: Tests for dense: too memory-consuming
     def test_hamiltonian(self):
         """
         Test the Hamiltonian generation against a previously calculated result.
         """
+        # Set the global parameters
+        sg.parameters.default()
+        sg.parameters.magnetic_field = 7e-3
         
         # Make the spin system
-        spins = np.array([1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 1/2, 1])
-        max_spin_order = 3
-        basis = make_basis(spins, max_spin_order)
+        ss = sg.SpinSystem(['1H', '1H', '1H', '1H', '1H', '1H', '1H', '14N'])
 
-        # Get the gyromagnetic ratios (in rad/s/T)
-        y_1H = 2*np.pi * ISOTOPES['1H'][1] * 1e6
-        y_14N = 2*np.pi * ISOTOPES['14N'][1] * 1e6
-        gammas = np.array([y_1H, y_1H, y_1H, y_1H, y_1H, y_1H, y_1H, y_14N])
+        # Construct the basis set
+        ss.basis.max_spin_order = 3
+        ss.basis.build()
 
         # Define chemical shifts (in ppm)
-        chemical_shifts = np.array(
-            [-22.7, -22.7, 8.34, 8.34, 7.12, 7.12, 7.77, 43.60])
+        ss.chemical_shifts = [-22.7, -22.7, 8.34, 8.34, 7.12, 7.12, 7.77, 43.60]
 
         # Define scalar couplings (in Hz)
-        J_couplings = np.array([\
+        ss.J_couplings = [
             [ 0,     0,      0,      0,      0,      0,      0,     0],
             [-6.53,  0,      0,      0,      0,      0,      0,     0],
             [ 0.00,  1.66,   0,      0,      0,      0,      0,     0],
@@ -40,74 +35,55 @@ class TestHamiltonian(unittest.TestCase):
             [ 0.38, -0.13,	 0.09,	 5.93,	 0.06, 	 0,      0,     0],			
             [ 0.01,	 0.03,	 1.12,	-0.02,	 7.75,  -0.01, 	 0,     0],
             [-0.30,  15.91,  4.47,   0.04,   1.79,   0,     -0.46,  0]
-        ])
-
-        # Set the magnetic field (7 mT)
-        B = 7e-3
-        
-        # Generate the Hamiltonian
-        H_comm = sop_H(
-            basis = basis,
-            spins = spins,
-            gammas = gammas,
-            B = B,
-            chemical_shifts = chemical_shifts,
-            J_couplings = J_couplings,
-            interactions = ["zeeman", "chemical_shift", "J_coupling"],
-            side = "comm",
-            sparse = True,
-            zero_value = 1e-12
-        )
-        
-        # Generate the same Hamiltonian again (check for cache errors etc.)
-        H_comm = sop_H(
-            basis = basis,
-            spins = spins,
-            gammas = gammas,
-            B = B,
-            chemical_shifts = chemical_shifts,
-            J_couplings = J_couplings,
-            interactions = ["zeeman", "chemical_shift", "J_coupling"],
-            side = "comm",
-            sparse = True,
-            zero_value = 1e-12
-        )
-
-        # Build left and right separately
-        H_left = sop_H(
-            basis = basis,
-            spins = spins,
-            gammas = gammas,
-            B = B,
-            chemical_shifts = chemical_shifts,
-            J_couplings = J_couplings,
-            interactions = ["zeeman", "chemical_shift", "J_coupling"],
-            side = "left",
-            sparse = True,
-            zero_value = 1e-12
-        )
-        H_right = sop_H(
-            basis = basis,
-            spins = spins,
-            gammas = gammas,
-            B = B,
-            chemical_shifts = chemical_shifts,
-            J_couplings = J_couplings,
-            interactions = ["zeeman", "chemical_shift", "J_coupling"],
-            side = "right",
-            sparse = True,
-            zero_value = 1e-12
-        )
+        ]
 
         # Load the previously calculated Hamiltonian for comparison
-        test_dir = os.path.dirname(os.path.dirname(__file__))
-        H_previous = load_npz(
-            os.path.join(test_dir, 'test_data', 'hamiltonian.npz'))
-
-        # Assert that the generated Hamiltonian matches the reference
+        test_dir = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'test_data'
+        )
+        H_previous = csc_array(
+            load_npz(os.path.join(test_dir, 'hamiltonian.npz'))
+        )
+        
+        # Generate the Hamiltonian using sparse and dense formalisms and compare
+        sg.parameters.sparse_hamiltonian = True
+        H_comm = sg.hamiltonian(ss)
         self.assertTrue(np.allclose(H_comm.toarray(), H_previous.toarray()))
+        # sg.parameters.sparse_hamiltonian = False
+        # H_comm = sg.hamiltonian(ss)
+        # self.assertTrue(np.allclose(H_comm, H_previous.toarray()))
 
-        # Assert that the commutation superoperator can be constructed from left
-        # and right
-        self.assertTrue(np.allclose((H_left - H_right).toarray(),
-                                    H_comm.toarray()))
+        # Perform the same test again (check for cache errors etc.)
+        sg.parameters.sparse_hamiltonian = True
+        H_comm = sg.hamiltonian(ss)
+        self.assertTrue(np.allclose(H_comm.toarray(), H_previous.toarray()))
+        # sg.parameters.sparse_hamiltonian = False
+        # H_comm = sg.hamiltonian(ss)
+        # self.assertTrue(np.allclose(H_comm, H_previous.toarray()))
+
+        # Perform the same test again but by building left and right separately
+        sg.parameters.sparse_hamiltonian = True
+        H_left = sg.hamiltonian(ss, side='left')
+        H_right = sg.hamiltonian(ss, side='right')
+        H_comm = H_left - H_right
+        self.assertTrue(np.allclose(H_comm.toarray(), H_previous.toarray()))
+        # sg.parameters.sparse_hamiltonian = False
+        # H_left = sg.hamiltonian(ss, side='left')
+        # H_right = sg.hamiltonian(ss, side='right')
+        # H_comm = H_left - H_right
+        # self.assertTrue(np.allclose(H_comm, H_previous.toarray()))
+
+        # Perform the same test again but build each interaction separately
+        sg.parameters.sparse_hamiltonian = True
+        H_comm_Z = sg.hamiltonian(ss, ["zeeman"])
+        H_comm_CS = sg.hamiltonian(ss, ["chemical_shift"])
+        H_comm_J = sg.hamiltonian(ss, ["J_coupling"])
+        H_comm = H_comm_Z + H_comm_CS + H_comm_J
+        self.assertTrue(np.allclose(H_comm.toarray(), H_previous.toarray()))
+        # sg.parameters.sparse_hamiltonian = False
+        # H_comm_Z = sg.hamiltonian(ss, ["zeeman"])
+        # H_comm_CS = sg.hamiltonian(ss, ["chemical_shift"])
+        # H_comm_J = sg.hamiltonian(ss, ["J_coupling"])
+        # H_comm = H_comm_Z + H_comm_CS + H_comm_J
+        # self.assertTrue(np.allclose(H_comm, H_previous.toarray()))
